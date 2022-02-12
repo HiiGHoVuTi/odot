7b35d28f708275aaa063508621f534713a2f938a
Maxime <emixampons@gmail.com>
2022-02-12 13:56:09 +0100

    feat: todo list tui

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,153 @@
+{-# LANGUAGE OverloadedStrings, TypeApplications, DataKinds, DeriveGeneric, RecordWildCards #-}
+
 module Main where
 
+import Brick
+import Brick.Widgets.Border
+import Brick.Widgets.Center
+import Brick.Widgets.Core
+import Brick.Widgets.Edit
+import Brick.Widgets.List
+import Control.Monad
+import Data.Function
+import Data.Time
+import Data.Generics.Product hiding (list)
+import GHC.Generics
+import Graphics.Vty.Attributes
+import qualified Graphics.Vty as V
+import qualified Graphics.Vty.Input.Events as K
+import Lens.Micro
+import qualified Options.Applicative as O
+import System.Directory
+
+-- PARSER
+
+data Options
+  = Options
+    {
+    }
+
+optparser :: O.Parser Options
+optparser = pure Options
+
 main :: IO ()
-main = putStrLn "Hello, Odot!"
+main = O.execParser optinfo >>= doTheThing
+  where
+    optinfo = O.info (optparser O.<**> O.helper)
+      $ O.fullDesc
+     <> O.progDesc (unlines
+        [
+        ]
+                 )
+     <> O.header "Odot -- a TODO TUI"
+
+
+-- FILES AND USER
+
+data ModelType = Exponential Double | Linear Double Double | Logarithmic Double
+  deriving (Show, Eq)
+
+data Todo
+  = Todo
+    { todoName :: String
+    , todoDue  :: UTCTime
+    , todoMod  :: ModelType
+    }
+  | Done
+    { todoName :: String
+    , todoDone :: UTCTime
+    , todoDue  :: UTCTime
+    , todoMod  :: ModelType
+    }
+
+baseDir :: IO FilePath 
+baseDir = fmap (<> "/.odot/") getHomeDirectory
+
+doTheThing :: Options -> IO ()
+doTheThing opts = do
+  baseDirExists <- doesDirectoryExist =<< baseDir
+  unless baseDirExists $ do
+     createDirectory =<< baseDir
+
+  let todoEditor = editor "Search" (Just 1) ""
+
+  now <- getCurrentTime 
+  
+  void $ defaultMain app $ AppState
+    { todos         = [ Todo "pet my cat" now (Linear 1 0) ]
+    , editorActive  = False
+    , addTodoEditor = todoEditor
+    }
+
+-- TUI
+
+data AppState
+  = AppState 
+    { todos         :: [Todo]
+    , addTodoEditor :: Editor String String
+    , editorActive  :: Bool
+    }
+  deriving Generic
+
+app :: App AppState () String
+app =
+  App
+  { appDraw = draw
+  , appChooseCursor = const.const Nothing
+  , appHandleEvent  = update
+  , appStartEvent   = pure
+  , appAttrMap      = const (attrMap defAttr 
+    [ (attrName "infoTitle", fg magenta)
+    ])
+  }
+
+draw :: AppState -> [Widget String]
+draw s = 
+  [ borderWithLabel (center $ title "Odot") $ vBox
+    [ {- border $ padAll 1 $
+      htitle "Add a todo: " <+> renderEditor drawEditor True (addTodoEditor s)
+      -}
+      padAll 1 $ hCenter $ border $ hCenter $ padTopBottom 2 $ str "WIP" 
+    , hCenter $ title "Here are your todos:"
+    , padAll 1 $ viewport "TodosViewport" Vertical 
+      $ vBox $ map drawTodo (todos s)
+    ]
+  ]
+    where
+      title t =
+        withAttr "infoTitle" $
+        txt t
+
+      drawEditor = foldl1 (<+>).map str
+
+      drawTodo Todo {..} = border $ padAll 1 $
+        str todoName <+> strWrap " " <+> str (formatTime defaultTimeLocale "%a, %d/%m, %l%P" todoDue)
+      drawTodo Done {  } = undefined
+
+update :: AppState -> BrickEvent String () -> EventM String (Next AppState)
+
+update s (VtyEvent (V.EvKey (K.KChar 'q') []))
+  | s & not.editorActive = halt s
+
+update s (VtyEvent (V.EvKey (K.KChar 'a') []))
+  | s & not.editorActive 
+  = s
+  & field @"editorActive" .~ True
+  & continue
+
+update s (VtyEvent (V.EvKey K.KEnter      [])) = s
+  & field @"editorActive" .~ False
+  & continue
+
+update s (VtyEvent e)
+  | editorActive s = do
+
+  editor' <- s 
+    & addTodoEditor 
+    & handleEditorEvent e
+  s 
+    & field @"addTodoEditor" .~ editor' 
+    & continue
+
+update s _ = continue s
+
--- odot.nix
+++ odot.nix
@@ -1,11 +1,16 @@
-{ mkDerivation, base, lib }:
+{ mkDerivation, base, brick, directory, generic-lens, lib
+, microlens, optparse-applicative, serialise, text, time, vty
+}:
 mkDerivation {
   pname = "odot";
   version = "0.1.0.0";
   src = ./.;
   isLibrary = false;
   isExecutable = true;
-  executableHaskellDepends = [ base ];
+  executableHaskellDepends = [
+    base brick directory generic-lens microlens optparse-applicative
+    serialise text time vty
+  ];
   description = "Odot is a todo-app made for its creator, and you too if your brain works the same way";
   license = "unknown";
   hydraPlatforms = lib.platforms.none;
